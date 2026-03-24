e6a47c7cff1026448e445f76b225e7644616a2b4
guilbartaubin-afk <guilbartaubin@yahoo.com>
2026-03-24 09:31:01 +0100

    premier TD

---
+++ TD1_main.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include "Header_TD1.c"
+
+int main(){
+    printf("Hello  TD1! \n");
+    int a = 10, b = 20;
+    //int max = compare(a, b);
+    //printf("Le maximum entre %d et %d est %d\n", a, b, max);
+    //selection();
+    //selection2();
+    var();
+    int s = sum(a, b);
+    printf("La somme de %d et %d est %d\n", a, b, s);
+    return 0;
+}
+
+
+
+
